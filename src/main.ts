4a46d8035863ded8dd2a3fcd5af7927187d06c39
장성원 <jangseong-won@jangseong-won-ui-MacBookPro.local>
2025-05-03 05:53:41 +0900

    cors 설정 추가

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,14 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  
+  // CORS 설정 추가 - 모든 도메인 허용
+  app.enableCors({
+    origin: '*',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    credentials: true,
+  });
+  
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
